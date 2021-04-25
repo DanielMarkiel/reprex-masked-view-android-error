0a198c020fa1ae030a474318dfc5476096ee9ba3
Daniel Markiel <daniel.markiel@gmail.com>
2021-04-25 09:29:12 +0200

    App crash reproduction

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from 'react-native-screens/native-stack';
+
+import HomeScreen from './src/scenes/Home';
+import MaskedViewScreen from './src/scenes/MaskedView';
+import {Button} from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <View>
-      <Text>Init</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="MaskedView"
+          component={MaskedViewScreen}
+          options={({navigation}) => ({
+            // Usage of custom 'headerLeft' breaks the app if MaskedView is rendered on the screen
+            headerLeft: () => (
+              <Button onPress={() => navigation.goBack()} title="Back" />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
